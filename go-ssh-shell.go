e4a50fcc9f844fe2dcb317554961ef710819b65f
faster-snail <34233121+faster-snail@users.noreply.github.com>
2018-10-30 18:08:52 +0800

    Update go-ssh-shell.go

--- go-ssh-shell.go
+++ go-ssh-shell.go
@@ -12,12 +12,15 @@ type mycli struct {
 	cli *ssh.Client
 }
 
+//此例是从https://godoc.org/golang.org/x/crypto/ssh 拷贝修改，将句柄创建封装
 func New(addr string,username string,password string) mycli {
 	client, err := ssh.Dial("tcp", addr, &ssh.ClientConfig{
 		User: username,
 		Auth: []ssh.AuthMethod{
 			ssh.Password(password),
 		},
+        //需要验证服务端，不做验证返回nil就可以，点击HostKeyCallback看源码就知道了
+		//如果按照官方文档操作出现问题，可以如下操作 		
 		HostKeyCallback: func(hostname string, remote net.Addr, key ssh.PublicKey) error {
 			return nil
 		},
@@ -28,7 +31,7 @@ func New(addr string,username string,password string) mycli {
 	cli := mycli{cli:client}
 	return cli
 }
-
+//对官方案例的命令执行过程封装成shell方法，此处为了使用cli句柄方法，在上面将ssh.Client类型封装为结构体
 func (client mycli)Myshell(cmd string) string {
 	session, err := client.cli.NewSession()
 	if err != nil {
@@ -45,11 +48,14 @@ func (client mycli)Myshell(cmd string) string {
 	}
 	return b.String()
 }
+
+
 func main() {
-	// Each ClientConn can support multiple interactive sessions,
-	// represented by a Session.
+	//（1）初始化一个cli句柄
+	//（2）执行shell
+	//（3）打印输出
 	cli := New("172.0.0.42:22","root","ybcsp.!QAZ")
-	str := cli.Myshell("uptime")
+	str := cli.Myshell("ls -l /")
 	fmt.Println(str)
 
 }
